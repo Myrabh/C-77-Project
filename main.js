087a631206d04a407fd29736b2413ce8eadbbf28
Myrabh <79832813+Myrabh@users.noreply.github.com>
2021-07-24 22:56:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log('My first console project');
+
+var str1 = "Tanay ";
+ var str2 = "Chatwani";
+ var res = str1.concat(str2);
+ console.log('My Name is :'+res);
+
+ var num1 = "10";
+ var num2 = "5";
+ 
+ console.log('Addition :'+(num1+num2));
+ console.log('Substraction :'+(num1-num2));
+ console.log('Multiplication :'+(num1*num2));
+ console.log('Division :'+(num1/num2));
